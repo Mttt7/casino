044a864d51521f0cabc3764fc706f1c6ebec1cc2
Michal Tomaszewski <groobytt@gmail.com>
2023-09-05 11:35:42 +0200

    add redirect to home page

--- casino/src/app/app-routing.module.ts
+++ casino/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { BlackjackComponent } from './games/blackjack/blackjack.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   {
     path: 'games', component: GamesComponent, children: [
